2905f88ccb9a9e5e1d2ebe6a2dde5c0180fe1f62
Cluck65 <harrykbenton@hotmail.com>
2022-01-28 13:02:07 +0000

    Add updater powershell script, remove .meta from .gitignore, scene improvements

---
+++ Installer.ps1
@@ -0,0 +1,4 @@
+﻿#Get release
+Invoke-WebRequest -Uri https://github.com/Cluck65/RPG/releases/download/rpg/PokemonYeah.zip -OutFile .\PokemonYeah.zip
+Expand-Archive .\PokemonYeah.zip -DestinationPath .\ -Force
+
